84a392b0483b1e9d91bb02ca59b8921d492d6a8c
Matthias Uschok <dev@uschok.de>
2019-12-26 19:42:07 +0100

    Add index and detail views

--- wishlist/wishlistapp/views.py
+++ wishlist/wishlistapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Wish, Wishlist
+
+
+def index(request):
+    list_of_wishlists = Wishlist.objects.all()
+    context = {'list_of_wishlists': list_of_wishlists}
+    return render(request, 'wishlistapp/index.html', context)
+
+
+def detail(request, wishlist_id):
+    wishlist = get_object_or_404(Wishlist, pk=wishlist_id)
+    return render(request, 'wishlistapp/wishlist.html', {'wishlist': wishlist})
